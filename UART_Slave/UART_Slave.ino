e6d39cb8c3fe1b3a55b304a26ee4cca051aef04f
venugopal1234567 <30863617+venugopal1234567@users.noreply.github.com>
2019-04-21 08:02:58 +0530

    Added Files

---
+++ UART_Slave/UART_Slave.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  Serial.println("Goodnight moon!");
+
+  
+}
+
+void loop() { // run over and over
+  Serial.println("Goodnight moon!");
+  delay(1000);
+ 
+}
